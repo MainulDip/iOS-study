cba34ea649b9e4ae391d481e939811d6a1cc5bf1
mainuldip <--global>
2023-05-10 15:06:06 -0700

    ios vertical centering examples

--- apps/LoginIntro/LoginIntro/LoginUIView.swift
+++ apps/LoginIntro/LoginIntro/LoginUIView.swift
@@ -14,15 +14,16 @@ struct LoginUIView: View {
     
     var body: some View {
         
-        VStack {
-            Spacer()
-    
+        VStack (alignment: .center) {
+        
             Section (header: Text("Login Form") ) {
+                
                 Form {
                     TextField("Email", text: $email)
                     SecureField("Password", text: $password)
                 }
             }
+            
             
             Section {
                 Button(action: {
@@ -34,8 +35,40 @@ struct LoginUIView: View {
             
             Spacer() // available ios 13.0 +
             
-        }
+        }.ignoresSafeArea()
     }
+    
+// Centering Vertical
+//    var body: some View {
+//            ZStack {
+//                Color.green
+//                    .edgesIgnoringSafeArea(.all)
+//
+//                VStack(spacing: 16) {
+//                    Color.clear
+//                        .overlay(
+//                            VStack {
+//                                Image(systemName: "clock")
+//                                    .resizable()
+//                                    .foregroundColor(.white)
+//                                    .aspectRatio(contentMode: .fit)
+//                                    .frame(width: 156, height: 80)
+//                                Color.clear
+//                                    .overlay(
+//                                        Text("My\nmultiline label")
+//                                            .multilineTextAlignment(.center)
+//                                            .font(.title)
+//                                            .foregroundColor(.white)
+//                                )
+//                            }
+//                        )
+//                    RoundedRectangle(cornerRadius: 5).fill(Color.white).frame(height: 79)
+//                    RoundedRectangle(cornerRadius: 5).fill(Color.white).frame(height: 79)
+//                    Color.clear
+//                }
+//                .padding([.leading, .trailing], 24)
+//            }
+//        }
 }
 
 struct LoginUIView_Previews: PreviewProvider {
