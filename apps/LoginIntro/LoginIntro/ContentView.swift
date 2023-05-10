baa91a2bf69c8a11b47ea7d5905fa0cac5e4f742
mainuldip <--global>
2023-05-09 19:00:19 -0700

    IOS login app init

---
+++ apps/LoginIntro/LoginIntro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginIntro
+//
+//  Created by kaka beautha on 5/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apps/LoginIntro/LoginIntro/LoginIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginIntroApp.swift
+//  LoginIntro
+//
+//  Created by kaka beautha on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
