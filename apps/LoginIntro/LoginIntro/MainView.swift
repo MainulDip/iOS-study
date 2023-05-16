96df7639c102346dbd502d4be349a9ef5df8a58d
mainuldip <--global>
2023-05-16 11:10:05 -0400

    ios-study adding sf-symbols app

--- apps/LoginIntro/LoginIntro/MainView.swift
+++ apps/LoginIntro/LoginIntro/MainView.swift
@@ -5,16 +5,16 @@ struct MainView: View {
         TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
             DashboardView().tabItem { Label(
                 title: { /*@START_MENU_TOKEN@*/Text("Label")/*@END_MENU_TOKEN@*/ },
-                icon: { /*@START_MENU_TOKEN@*/Image(systemName: "42.circle")/*@END_MENU_TOKEN@*/ })
+                icon: { Image(systemName: "house.circle") })
             }
             ProfileView().tabItem { Label(
                 title: { /*@START_MENU_TOKEN@*/Text("Label")/*@END_MENU_TOKEN@*/ },
-                icon: { /*@START_MENU_TOKEN@*/Image(systemName: "42.circle")/*@END_MENU_TOKEN@*/ })
+                icon: { Image(systemName: "person") })
             }
             SettingsView().tabItem {
                 Label(
                     title: { /*@START_MENU_TOKEN@*/Text("Label")/*@END_MENU_TOKEN@*/ },
-                    icon: { /*@START_MENU_TOKEN@*/Image(systemName: "42.circle")/*@END_MENU_TOKEN@*/ })
+                    icon: { Image(systemName: "gear") })
             }
         }
     }
