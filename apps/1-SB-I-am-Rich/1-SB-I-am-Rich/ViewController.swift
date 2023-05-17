c1a5ba6f15d876182c0ebacb0f0366c02fa6fd8d
mainuldip <--global>
2023-05-17 11:46:52 -0400

    ios-study storyboard I am Rich App init

---
+++ apps/1-SB-I-am-Rich/1-SB-I-am-Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1-SB-I-am-Rich
+//
+//  Created by kaka beautha on 5/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
