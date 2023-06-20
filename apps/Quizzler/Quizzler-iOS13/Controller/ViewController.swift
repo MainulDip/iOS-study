fa47994962246e2bbdef1cbf1913296a27f76acf
mainuldip <mainuldip@gmail.com>
2023-06-20 17:32:37 -0400

    ios-study mvc implementtion and use of computed properties

--- apps/Quizzler/Quizzler-iOS13/Controller/ViewController.swift
+++ apps/Quizzler/Quizzler-iOS13/Controller/ViewController.swift
@@ -16,24 +16,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnFalse: UIButton!
     
     
-    var currentQuestion = 0
+    private var questionStore = QuestionStore()
+    
+    // computed properties
+    var questions: [Question] {
+        get { questionStore.questions }
+    }
+    
+    
+    // computed properties
+    var currentQuestion: Int {
+        get { questionStore.currentQuestion }
+        set { questionStore.currentQuestion = newValue }
+    }
     var timer = Timer()
     
-    
-    // Questions Storage
-    var questions: [Question] = [  Question(q: "A slug's blood is green.", a: "True"),
-                                   Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-                                   Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-                                   Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-                                   Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-                                   Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-                                   Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-                                   Question(q: "Google was originally called 'Backrub'.", a: "True"),
-                                   Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-                                   Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-                                   Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-                                   Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-    		]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -47,6 +44,8 @@ class ViewController: UIViewController {
         
         let questionAns = questions[currentQuestion].answer
         let btn = sender.titleLabel?.text
+        
+        
         
         // Check if anser is correct
         if (btn == questionAns){
@@ -78,7 +77,7 @@ class ViewController: UIViewController {
             print("called2")
         }
         qText.text = questions[currentQuestion].text
-        examProgress.progress = Float(currentQuestion) / Float(questions.count)
+        examProgress.progress = Float(currentQuestion) / Float(questions.count - 1)
         btnTrue.backgroundColor = UIColor.clear
         btnFalse.backgroundColor = UIColor.clear
         
