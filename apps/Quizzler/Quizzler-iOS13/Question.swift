ce470cd2d4182d28545ceaa75e52f588571ed3b3
mainuldip <mainuldip@gmail.com>
2023-06-19 23:16:17 -0400

    ios-study Quiz App docking yet MVC Implementation

--- apps/Quizzler/Quizzler-iOS13/Question.swift
+++ apps/Quizzler/Quizzler-iOS13/Question.swift
@@ -11,4 +11,9 @@ import Foundation
 struct Question {
     var text: String
     var answer: String
+    
+    init(q: String, a: String) {
+        self.text = q
+        self.answer = a
+    }
 }
--- apps/Quizzler/Quizzler-iOS13/ViewController.swift
+++ apps/Quizzler/Quizzler-iOS13/ViewController.swift
@@ -17,18 +17,30 @@ class ViewController: UIViewController {
     
     
     var currentQuestion = 0
+    var timer = Timer()
     
     
     // Questions Storage
-    var questions: [Question] = [Question(text: "1 + 2 = 3", answer: "True"),
-                                 Question(text: "2 + 2 = 2", answer: "False"),
-                                 Question(text: "3 + 4 = 7", answer: "True")]
+    var questions: [Question] = [  Question(q: "A slug's blood is green.", a: "True"),
+                                   Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+                                   Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+                                   Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+                                   Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+                                   Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+                                   Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+                                   Question(q: "Google was originally called 'Backrub'.", a: "True"),
+                                   Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+                                   Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+                                   Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+                                   Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+    		]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // input question form store
         qText.text = questions[currentQuestion].text
+        examProgress.progress = 0
     }
 
     @IBAction func ansBtnPressed(_ sender: UIButton) {
@@ -39,29 +51,36 @@ class ViewController: UIViewController {
         // Check if anser is correct
         if (btn == questionAns){
             print("Answer is corect")
+            sender.backgroundColor = UIColor.green
         } else {
             print("Answer is in-correct")
+            sender.backgroundColor = UIColor.red
         }
         
         if (btn == "True"){
             print("True Pressed")
             // Show next
-            nextQuestion()
+//            nextQuestion()
         } else {
             print("False Pressed")
             // Show next
-            nextQuestion()
+//            nextQuestion()
         }
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(nextQuestion), userInfo: nil, repeats: false)
     }
     
-    func nextQuestion() {
+    @objc func nextQuestion() {
         currentQuestion += 1
-        print(currentQuestion, questions.count)
+        print(currentQuestion, questions.count, (Float(currentQuestion) / Float(questions.count)))
         if (currentQuestion > (questions.count - 1)) {
             currentQuestion = 0
             print("called2")
         }
         qText.text = questions[currentQuestion].text
+        examProgress.progress = Float(currentQuestion) / Float(questions.count)
+        btnTrue.backgroundColor = UIColor.clear
+        btnFalse.backgroundColor = UIColor.clear
         
         
     }
