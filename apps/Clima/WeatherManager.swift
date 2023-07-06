6cc77db38299bcbbc575700049ea3d1432ab0fbe
mainuldip <mainuldip@gmail.com>
2023-07-06 02:24:19 -0400

    ios-study openweathermap api init

---
+++ apps/Clima/WeatherManager.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Mainul Dip on 7/6/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric"
+    
+    func fetchWeatherByCity(_ cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        print(urlString)
+    }
+}
