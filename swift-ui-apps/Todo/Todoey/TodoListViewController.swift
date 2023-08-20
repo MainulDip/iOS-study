b53f0617d7a62602edb4afac0092a336a4fad4dd
mainuldip <mainuldip@gmail.com>
2023-08-20 03:18:34 -0400

    ios-study uikit todo app tableview init

--- swift-ui-apps/Todo/Todoey/TodoListViewController.swift
+++ swift-ui-apps/Todo/Todoey/TodoListViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Item 1", "Item 2", "Item 3" ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
     }
 }
 
--- xcode-overview.md
+++ xcode-overview.md
@@ -42,3 +42,6 @@ Docs : https://www.swiftdevjournal.com/using-environment-variables-in-swift-apps
 ### Simulator Location Customizing
 It's in Debug -> Simulator Location
 Custom Location can be set to debug different things, like CoreLocations
+
+### Code Scenes:
+L: Local Variable (also maybe passed when extending another class)
