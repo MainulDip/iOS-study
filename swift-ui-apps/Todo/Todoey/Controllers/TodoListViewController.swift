ed85e8299fbbedc1d21dfa7b483136c26e408a4b
mainuldip <mainuldip@gmail.com>
2023-08-24 03:49:49 -0400

    ios-study UserDefaults object storage bug

--- swift-ui-apps/Todo/Todoey/Controllers/TodoListViewController.swift
+++ swift-ui-apps/Todo/Todoey/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Item 1", "Item 2", "Item 3" ]
+    var itemArray: [Item] = []
     
     // Persistance local sotrage using UserDefaults
     let defaults = UserDefaults.standard
@@ -18,7 +18,7 @@ class TodoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // load local data
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        if let items = defaults.object(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
         
@@ -36,7 +36,7 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
         
         return cell
     }
@@ -67,7 +67,8 @@ class TodoListViewController: UITableViewController {
             
             if let text = textField.text {
                 DispatchQueue.main.async {
-                    self.itemArray.append(text)
+                    let newItem = Item(title: text)
+                    self.itemArray.append(newItem)
                     
                     // update to the persistance UserDefaults
                     self.defaults.set(self.itemArray, forKey: "TodoListArray")
---
+++ swift-ui-apps/Todo/Todoey/Models/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mainul Dip on 8/23/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String
+    var done: Bool = false
+    
+    init(title: String){
+        self.title = title
+    }
+}
