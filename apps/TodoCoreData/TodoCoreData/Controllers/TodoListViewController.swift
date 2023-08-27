0ccf629c616643d64343e4fb7470b53fbc3393a4
mainuldip <mainuldip@gmail.com>
2023-08-26 22:08:54 -0400

    ios-study coredata init

---
+++ apps/TodoCoreData/TodoCoreData/Controllers/TodoListViewController.swift
@@ -0,0 +1,155 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    var itemArray: [Item] = []
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+    
+    // Persistance local sotrage using UserDefaults
+    let defaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Our DataFilePath", FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        print(dataFilePath ?? "dataFilePant is nil lol")
+        
+        // load local data
+//        if let url = dataFilePath {
+//            decodeItemData(fitePath: url)
+//        }
+        
+        // get local data and update the local list using generics
+        if let url = dataFilePath {
+            let sth: [Item] = decodeItemDataGenerics(filePath: url)
+            itemArray = sth
+        }
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    // MARK - TableView Cell Bindings
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListCell", for: indexPath)
+        
+        let theItem = itemArray[indexPath.row]
+        cell.textLabel?.text = theItem.title
+        cell.accessoryType = theItem.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    // MARK - TableView Delegates Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("The selected row is \(indexPath.row)")
+        
+        // seleced row flash effect
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // toggle the object prop
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        // encode and store local data using NSCoder
+        self.encodeAndStore(localData: self.itemArray, filePath: self.dataFilePath!)
+        tableView.reloadData()
+    }
+    
+    // MARK - Add New Item
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        print("adding item")
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            print("Success")
+            
+            if let text = textField.text {
+                DispatchQueue.main.async {
+                    let newItem = Item(title: text)
+                    self.itemArray.append(newItem)
+                    
+                    // update to the persistance UserDefaults
+//                    self.defaults.set(self.itemArray, forKey: "TodoListArray")
+                    
+                    // using NSCoder
+                    self.encodeAndStore(localData: self.itemArray, filePath: self.dataFilePath!)
+                    
+                    
+                    // reload the tableView
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
+        
+        // set alert text field placeholder text and get the alertTextField data
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new Item"
+            
+            // populate data to use other scope
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    // MARK - Custom NSCoder Encoder and Decoder Function
+    
+    func encodeAndStore(localData: Encodable, filePath: URL) {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(localData)
+            try data.write(to: filePath)
+            
+        } catch {
+            print("Custom PropertyList Encoder: ", error)
+        }
+    }
+    
+    func decodeItemData (fitePath: URL) {
+        let decoder = PropertyListDecoder()
+        
+        do {
+            let data = try Data(contentsOf: fitePath)
+            itemArray = try decoder.decode([Item].self, from: data)
+        } catch {
+            print("Decoder Error: ", error)
+        }
+    }
+    
+    // over-engineering using generics
+    func decodeItemDataGenerics<T: Decodable> (filePath: URL) -> [T] {
+        let decoder = PropertyListDecoder()
+        var storedItemArray: [T] = []
+        
+        do {
+            let data = try Data(contentsOf: filePath)
+            storedItemArray = try decoder.decode([T].self, from: data)
+        } catch {
+            print("Decoder Error: ", error)
+        }
+        
+        return storedItemArray
+    }
+    
+}
+
--- ios-storage.md
+++ ios-storage.md
@@ -1,10 +1,15 @@
 ## Overview:
 Jump starting guide on Local Storage and Persistance Storage etc
+- UserDefaults
+- KeyChain
+- NSCoder/Codable
+- CoreData/Sqlite
+_ Realm (OpenSource/Trending)
 
 ### AppDelegate lifecycle Methods and App Cache Lifecycle:
 - applicationDidEnterBackground :
 - applicationWillTerminate: call last when cleared by os or manually form background. All cached app data will be destroyed.
-### UserDefaults (Persistent Local Data Storage):
+### UserDefaults (Persistent Local Data Storage) [Todo App]:
 - UserDefaults save data as "plist" file.
 - Not good for big data (Slow because ios need to search and load all the local plist file), so better Only store small data (few KBs)
 - Its not a database replacement
@@ -37,8 +42,89 @@ print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, t
 ### Singleton in Swift:
 - Kinda same as Java and other programming languages, use static property to initialize and get the object.
 
-### Storing Custom Objects Using NSCoder:
+### Storing Custom Objects Using NSCoder [Todo App]:
 To store custom objects UserDefaults show it's limitation. As UserDefault store data as property lists (key value pair dictionary), any object besides form built in types need the NSCoder Route.
 
 To Use NSCoder to store custom objects into the plist file, we need
-- url, where the data will be stored.
+- define the url using FileManager, where the data will be stored, and get the handle
+- encode the data with PropertyListEncoder and save the encoded data to the url location
+- decode the stored data with PropertyListDecoder, the model structure/type and the stored filePath needs to be supplied
+
+```swift
+class TodoListViewController: UITableViewController {
+    
+    var itemArray: [Item] = []
+    
+    // define the file which will be used to store data
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // get local data and update the local list using generics
+        if let url = dataFilePath {
+            let sth: [Item] = decodeItemDataGenerics(filePath: url)
+            itemArray = sth
+        }
+    }
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+
+        // using NSCoder
+        self.encodeAndStore(localData: self.itemArray, filePath: self.dataFilePath!)
+        
+        
+        // reload the tableView
+        self.tableView.reloadData()
+
+    }
+    
+    // MARK - Custom NSCoder Encoder and Decoder Function
+    
+    func encodeAndStore(localData: Encodable, filePath: URL) {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(localData)
+            try data.write(to: filePath)
+            
+        } catch {
+            print("Custom PropertyList Encoder: ", error)
+        }
+    }
+    
+    func decodeItemData (fitePath: URL) {
+        let decoder = PropertyListDecoder()
+        
+        do {
+            let data = try Data(contentsOf: fitePath)
+            itemArray = try decoder.decode([Item].self, from: data)
+        } catch {
+            print("Decoder Error: ", error)
+        }
+    }
+    
+    // over-engineering using generics
+    func decodeItemDataGenerics<T: Decodable> (filePath: URL) -> [T] {
+        let decoder = PropertyListDecoder()
+        var storedItemArray: [T] = []
+        
+        do {
+            let data = try Data(contentsOf: filePath)
+            storedItemArray = try decoder.decode([T].self, from: data)
+        } catch {
+            print("Decoder Error: ", error)
+        }        
+        return storedItemArray
+    }    
+}
+```
+### Storage with CoreData:
+- Adding CoreData:
+ - add new file, select CoreData Section's Data Model
+ - add all the AppDelegate functions for CoreData, NSPersistentContainer name should match with the newly created DataModel filename. (Don't forget to add/import the CoreData package)
+
+* CoreData Terms:
+- Entity (Class)
+- Attributes (Property)
+- Entities are table of data
