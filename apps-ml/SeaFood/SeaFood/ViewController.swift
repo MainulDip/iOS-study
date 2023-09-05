9a56892fa828ffc3b10a87a35771707cafa0424a
mainuldip <mainuldip@gmail.com>
2023-09-05 18:56:26 -0400

    ios-study coreml image pickers

--- apps-ml/SeaFood/SeaFood/ViewController.swift
+++ apps-ml/SeaFood/SeaFood/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // bind delegates
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] {
+            imageView.image = (userPickedImage as? UIImageView)?.image
+            print("User Image Selected \(imageView?.image?.size)")
+        }
+        
+        imagePicker.dismiss(animated: true)
     }
 
-
+    @IBAction func checkImage(_ sender: UIBarButtonItem) {
+        print("Check btn pressed")
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
