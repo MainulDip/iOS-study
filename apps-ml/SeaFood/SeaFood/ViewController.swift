e7f3bc061e22bf97202484a2de1a7e5a67960219
mainuldip <mainuldip@gmail.com>
2023-09-05 16:46:14 -0400

    ios-study creml  seafood init

---
+++ apps-ml/SeaFood/SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Mainul Dip on 9/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
