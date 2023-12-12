77854e266776ca3c38faa6e74bc190f1a1e5743d
mainuldip <mainuldip@gmail.com>
2023-12-12 01:34:21 -0500

    SwiftUI-100-Days WeSplit Init

---
+++ SwiftUI-100-Days/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mainul Dip on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
