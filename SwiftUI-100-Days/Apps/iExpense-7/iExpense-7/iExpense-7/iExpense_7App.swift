2295ab10739ba7be40225f75e51a44e286f7d37b
mainuldip <mainuldip@gmail.com>
2023-12-18 22:41:30 -0500

    ios-study swiftui iExpense init

---
+++ SwiftUI-100-Days/Apps/iExpense-7/iExpense-7/iExpense-7/iExpense_7App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_7App.swift
+//  iExpense-7
+//
+//  Created by Mainul Dip on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
