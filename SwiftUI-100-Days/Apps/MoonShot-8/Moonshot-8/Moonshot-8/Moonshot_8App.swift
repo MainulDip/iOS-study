477e1f04a6cc177a05ea424671a1f2f4891c05f0
mainuldip <mainuldip@gmail.com>
2024-01-03 16:18:59 -0500

    SwiftUI MoonShot App Init

---
+++ SwiftUI-100-Days/Apps/MoonShot-8/Moonshot-8/Moonshot-8/Moonshot_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_8App.swift
+//  Moonshot-8
+//
+//  Created by Mainul Dip on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
