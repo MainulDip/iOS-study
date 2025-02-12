e4dca1b195d0ff950d2d8bb2abdd93248dfc5384
MainulDip <mainuldip@gmail.com>
2025-02-12 04:11:40 -0500

    ios UICollectionView app Init

---
+++ apps-uikit-no-interface-builder/UICollectionView-Programmatic-Intro/UICollectionView-Programmatic-Intro/UICollectionView-Programmatic-Intro/CustomViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CustomViewController.swift
+//  UICollectionView-Programmatic-Intro
+//
+//  Created by Mainul Dip on 2/12/25.
+//
+
+import UIKit
+
+class CustomViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
