5405139085072c3deaa97cf66e23065d9254d227
MainulDip <mainuldip@gmail.com>
2025-04-07 21:57:12 -0400

    ios study gcd in depth init app

---
+++ apps-mixed-uikit-swiftui/Grand-Central-Dispatch-InDepth/GCD-InDepth-First-UIKit/GCD-InDepth-First-UIKit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GCD-InDepth-First-UIKit
+//
+//  Created by Mainul Dip on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .orange
+    }
+
+
+}
+
