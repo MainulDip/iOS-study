5b3e9f7652b9169f611d2d484d4fa2086ce0bb01
MainulDip <mainuldip@gmail.com>
2025-04-21 21:54:35 -0400

    ios combine networking intros

---
+++ apps-mixed-uikit-swiftui/Combine-Reactive-Framework-All-About/Combine-Networking-Basics-3nd/Combine-Networking-Basics-3nd/Combine_Networking_Basics_3ndApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_Networking_Basics_3ndApp.swift
+//  Combine-Networking-Basics-3nd
+//
+//  Created by Mainul Dip on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Combine_Networking_Basics_3ndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
