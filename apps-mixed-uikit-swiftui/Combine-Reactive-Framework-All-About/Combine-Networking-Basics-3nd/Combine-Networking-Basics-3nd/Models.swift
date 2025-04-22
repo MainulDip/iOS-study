3fa7d1332976081a745ade85378a44ae4cb6f9c2
MainulDip <mainuldip@gmail.com>
2025-04-22 05:01:47 -0400

    ios combine networking init

---
+++ apps-mixed-uikit-swiftui/Combine-Reactive-Framework-All-About/Combine-Networking-Basics-3nd/Combine-Networking-Basics-3nd/Models.swift
@@ -0,0 +1,54 @@
+//
+//  Movies.swift
+//  Combine-Networking-Basics-3nd
+//
+//  Created by Mainul Dip on 4/22/25.
+//
+
+import Foundation
+
+let jsonDecoder: JSONDecoder = {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    return decoder
+}()
+
+// MARK: - Movies
+
+struct Movie: Decodable, Equatable, Identifiable {
+    let id: Int
+    let title: String
+    let overview: String
+    let posterPath: String?
+    var posterURL: URL? {
+        posterPath.map { URL(string: "https://image.tmdb.org/t/p/w500\($0)")! }
+    }
+}
+
+struct MovieResponse: Decodable {
+    let results: [Movie]
+}
+
+// MARK: - Credits
+
+struct MovieCastMember: Identifiable, Equatable, Decodable {
+    let id: Int
+    let name: String
+    let character: String
+}
+
+struct MovieCreditsResponse: Decodable {
+    let cast: [MovieCastMember]
+}
+
+// MARK: - Reviews
+
+struct MovieReview: Identifiable, Equatable, Decodable {
+    let id: Int
+    let author: String
+    let content: String
+}
+
+struct MovieReviewsResponse: Decodable {
+    let results: [MovieReview]
+}
---
+++ apps-mixed-uikit-swiftui/Combine-Reactive-Framework-All-About/Combine-Networking-Basics-3nd/Combine-Networking-Basics-3nd/Network.swift
@@ -0,0 +1,21 @@
+//
+//  Network.swift
+//  Combine-Networking-Basics-3nd
+//
+//  Created by Mainul Dip on 4/22/25.
+//
+
+import Foundation
+import Combine
+
+func fetchMovies() -> AnyPublisher<MovieResponse, Error> {
+    // TODO : Add Api Key From Movie Database
+    let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=YOUR_API_KEY&language=en-US&page=1")!
+    
+    return URLSession
+        .shared
+        .dataTaskPublisher(for: url)
+        .map(\.data)
+        .decode(type: MovieResponse.self, decoder: JSONDecoder())
+        .eraseToAnyPublisher()
+}
